7f454a9f5ce4cd4dbba09acb1a8a856468f864c2
Cpierse <Christopher.Pierse@gmail.com>
2017-04-14 16:41:29 -0700

    Added data and functionality or 2017 recs

--- flaskgui/compute.py
+++ flaskgui/compute.py
@@ -3,7 +3,7 @@ import json
 
 def compute(role,champ):
     """Return relevant champ rec dictionary"""
-    with open('Recs2016.json', 'r') as fp:
+    with open('Recs2017.json', 'r') as fp:
         recs = json.load(fp)
     return recs['GOLD'][role][champ]
 
